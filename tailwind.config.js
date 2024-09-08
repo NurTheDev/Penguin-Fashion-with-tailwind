b4c29f6e0760215577be6761c8523d5f1d9e7dbd
nurislam <nur756.islam@gmail.com>
2024-09-09 00:06:21 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        gold: "#FABE4C;",
+        gray: "#3E3E3E",
+      },
+      fontFamily: {
+        babes: "'Bebas Neue', sans-serif",
+        robot: "'Roboto', sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
